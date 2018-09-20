693ec94597dfe8eef1b04201457d1ba07d64d3f7
basch1727 <basch19900@gmail.com>
2018-09-20 23:19:40 +0300

    edit

--- src/index.js
+++ src/index.js
@@ -4,4 +4,18 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
-};
+  var spc =0;
+  for (var a=0, lenght =preferences.length; a < lenght;  a++) {
+    var spc1=preferences[preferences[preferences[a]-1]-1]=== a+1;
+    var spc2=a+1 !==preferences[a];
+
+    if (spc1 && spc2){
+     
+        spc++;
+    }
+    }
+    return spc /3;
+  };
+
+
+
